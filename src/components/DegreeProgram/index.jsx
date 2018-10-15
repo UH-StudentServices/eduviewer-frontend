0b374be6074d6c8ffc8750ed8abfd1da4d5481e7
eero3 <eero.siren@kolumbus.fi>
2018-10-15 14:20:54 +0300

    changed DegreeProgram component to be stateless component

--- src/components/DegreeProgram/index.jsx
+++ src/components/DegreeProgram/index.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { bool } from 'prop-types';
 import { degreeProgramType } from '../../types';
 
@@ -6,34 +6,30 @@ import GroupingModule from '../GroupingModule';
 
 import styles from './degreeProgram.css';
 
-class DegreeProgram extends PureComponent {
-  static propTypes = {
-    degreeProgram: degreeProgramType.isRequired,
-    showAll: bool.isRequired,
-    showContent: bool.isRequired
-  };
-
-  render() {
-    const { showAll, showContent, degreeProgram } = this.props;
-
-    if (!showContent) {
-      return null;
-    }
-
-    const { name, dataNode } = degreeProgram;
-
-    return (
-      <div className={styles.degreeProgram}>
-        <h3>{name.fi}</h3>
-        <div className={styles.moduleGroups}>
-          <GroupingModule
-            key={dataNode.rule.localId}
-            rule={dataNode.rule}
-            showAll={showAll}
-          />
-        </div>
-      </div>);
+const DegreeProgram = ({ showAll, showContent, degreeProgram }) => {
+  if (!showContent) {
+    return null;
   }
-}
+
+  const { name, dataNode } = degreeProgram;
+
+  return (
+    <div className={styles.degreeProgram}>
+      <h3>{name.fi}</h3>
+      <div className={styles.moduleGroups}>
+        <GroupingModule
+          key={dataNode.rule.localId}
+          rule={dataNode.rule}
+          showAll={showAll}
+        />
+      </div>
+    </div>);
+};
+
+DegreeProgram.propTypes = {
+  degreeProgram: degreeProgramType.isRequired,
+  showAll: bool.isRequired,
+  showContent: bool.isRequired
+};
 
 export default DegreeProgram;
